7a195a040141d8655070a092fa1dcbad5a72bcc5
Neetin Kumar <kumarneetin323@gmail.com>
2022-03-15 00:18:42 +0530

    Fully functional 'translate' App using Context.consumer too

--- React/translate/src/components/App.js
+++ React/translate/src/components/App.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+
 
 const App = () => {
 
     const [language, setLanguage] = useState('english');
+    const [bgCol, setBgCol] = useState('#000000');
+    const [txtCol, setTxtCol] = useState('#ffffff');
 
     return (
         <div className="ui container">
@@ -13,9 +17,29 @@ const App = () => {
                 <i className="flag us" onClick={()=> setLanguage('english')} style={{ cursor: 'pointer' }} />
                 <i className="flag nl" onClick={()=> setLanguage('hindi')} style={{ cursor: 'pointer' }} />
             </div>
-            <LanguageContext.Provider value={language}>
-                <UserCreate />
-            </LanguageContext.Provider>
+            <div>
+                Select Background color of button : 
+                <input 
+                    type='color' 
+                    value={bgCol} 
+                    onChange={e => setBgCol(e.target.value)}
+                    style={{ cursor: 'pointer' }}
+                />
+            </div>
+            <div>
+                Select Text color of button : 
+                <input 
+                    type='color' 
+                    value={txtCol} 
+                    onChange={e => setTxtCol(e.target.value)}
+                    style={{ cursor: 'pointer' }}
+                />
+            </div>
+            <ColorContext.Provider value={{ bgCol, txtCol }}>
+                <LanguageContext.Provider value={language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
+            </ColorContext.Provider>
         </div>
     );
 };
--- React/translate/src/components/Button.js
+++ React/translate/src/components/Button.js
@@ -1,20 +1,40 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
-class Button extends React.Component {
 
-    static contextType = LanguageContext;
+const Button = () => {
 
-    render() {
 
-        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    const renderButtonText = value => value === 'english' ? 'Submit' : 'Voorleggen';
 
-        return (
-            <button className="ui button primary">
-                {text}
-            </button>
-        );
-    }
-}
+    const renderColoredButton = btnColor => (
+        <button className={`ui button`} style={{color: btnColor.txtCol, backgroundColor: btnColor.bgCol}}>
+            <LanguageContext.Consumer>
+                {renderButtonText}
+            </LanguageContext.Consumer>
+        </button>
+    );
+
+    return (
+        <ColorContext.Consumer>
+            {renderColoredButton}
+        </ColorContext.Consumer>
+    );
+};
+
+// class Button extends React.Component {
+
+//     render() {
+
+//         const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+
+//         return (
+//             <button className="ui button pink">
+//                 {text}
+//             </button>
+//         );
+//     }
+// }
 
 export default Button;
